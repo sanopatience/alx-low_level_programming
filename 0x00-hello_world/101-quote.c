cb0f7cf045ae2b5fb5ec99f5e0a57a5344364c14
sanopatience <patiencesatnam@gmail.com>
2022-10-13 10:13:38 -0700

    my second commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  *Description: main -Entry
- *Return: returns 0
+ *Return: returns 1
  */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora korpar, 2015-10-19\"));
+sizeof("and that piece of art is useful\" - Dora korpar, 2015-10-19));
 return (1);
 }
