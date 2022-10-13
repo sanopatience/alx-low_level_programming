6d9e41739c759bc5cf76bb402d9ebbaa30b8b1b9
sanopatience <patiencesatnam@gmail.com>
2022-10-13 09:26:01 -0700

    My first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description: main -Entry.
+ * Return: returns 0.
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", size of (char));
+printf("Size of an int: %d byte(s)\n", size of(int));
+printf("Size of a long int: %d byte(s)\n", size of(long int));
+printf("Size of a long long int: %d byte(s)\n", size of (long long int));
+printf("Size of a float: %d byte(s)\n", size of(float));
+return(0);
+}
