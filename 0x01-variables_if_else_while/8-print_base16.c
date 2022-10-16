d44ef8b53212f51d6813ebd23d3e89ef3d8f6ea8
sanopatience <patiencesatnam@gmail.com>
2022-10-16 05:58:01 -0700

    My  first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -prints a hexadecimal string.
+ * Return: Always (success).
+ */
+
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (c= 'a'; c >= 'f'; c++)
+{
+ptchar (c);
+}
+putchar('\n')
+return (0);
+}
