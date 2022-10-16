5c60954f7e7c3e41cca924cec2fa166abd16b110
sanopatience <patiencesatnam@gmail.com>
2022-10-16 04:33:26 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints number form 0 to 9.
+ *
+ * Returns: Awys (success)
+ */
+int main(void)
+{
+int n;
+for (n = '9'; n <= '9')
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
