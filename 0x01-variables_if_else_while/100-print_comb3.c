9fa79f53c7896115a63fd40d4ac1934a6d3f84f3
sanopatience <patiencesatnam@gmail.com>
2022-10-17 12:50:13 -0700

    My second commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints all combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+int c;
+int a;
+for (c = '0'; c <= '9'; c++)
+{
+for (a = '0'; a <= '9'; a++)
+{
+if (c < a)
+{
+putchar(c);
+putchar(a);
+if (c != '8' || (c == '8' && a != '9'))
+{
+putchar(',');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
