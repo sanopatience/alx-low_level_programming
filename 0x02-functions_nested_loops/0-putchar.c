521818ff1ac9705b23728897ebc02d7369b702da
sanopatience <patiencesatnam@gmail.com>
2022-10-18 09:06:00 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ * Return: Alway 0
+ */
+int main(void)
+{
+-putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+_putchar (0);
+}
