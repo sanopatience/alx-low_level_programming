48581df2995e1951a87f5e90b5c2cb6e56e2911b
sanopatience <patiencesatnam@gmail.com>
2022-10-20 10:13:47 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * @c: variable text
+ * Return: 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
