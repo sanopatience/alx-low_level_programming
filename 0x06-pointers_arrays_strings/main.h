8069971269f9d32f5f201693ec8e47140ddbd876
sanopatience <patiencesatnam@gmail.com>
2022-10-28 13:01:31 -0700

    my second commit for main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ char *string_toupper(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 int _putchar(char c);
-
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
 
