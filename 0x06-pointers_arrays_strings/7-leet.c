0a4a66fa304acf851133184aaae5ec707d51cc0a
sanopatience <patiencesatnam@gmail.com>
2022-10-28 11:28:36 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,23 @@
 
 /**
  * leet - encodes a string into 1337
- * @n: input
- * Return: the value of n
+ * @s: string to encode
+ * Return: address of s
  */
 
-char *leet(char *n)
+char *leet(char *s)
 {
-int i;
-int j;
-char s1[] = "aAeEoOtTlL";
-char s2[] = "4433007711"
+int i, j;
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
 
-for (i = 0; n[i] != '\0'; i++)
+for (i = 0; *(s + i); i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j <= 9; j++)
 {
-if (n[i] == s1[j])
-{
-n[i] = s2[j];
+if (a[j] == *(s + i))
+*(s + i) = b[j];
 }
 }
-}
-return (n);
+return (s);
 }
