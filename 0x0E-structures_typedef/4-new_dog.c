cb62fe87e1349680303aa1a70e374c5771acbaef
sanopatience <patiencesatnam@gmail.com>
2022-11-16 05:17:09 -0800

    Task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include  "dog.h"
-
 char *_strdup(char *str);
 
 /**
@@ -36,3 +35,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	return (new_d);
 }
+
+/**
+ * _strdup - function with one argument
+ * @str: string argument
+ *  Description: returns a pointer to allocated space in memory
+ *   Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
+
